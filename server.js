515adab18cf9ba82daed04f9fd63cb594cdfff6d
jsmwenjingzhang <wenjing_zh@sina.com>
2018-12-04 11:59:52 +0800

    init

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const path = require('path'),
 	appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
 	logger = require('@finelets/hyper-rest/app/Logger');
 
-var app = appBuilder.getApp();
-
 appBuilder
 	.setWebRoot('/root', './client')
 	.setFavicon('client/imgs/favicon.jpg')
 	.end();
 
-//connectDb(function () {
+connectDb(function () {
 	logger.info('connect mongodb success .......');
 	var server = appBuilder.run(function () {
 		const addr = server.address();
 		logger.info('the server is running and listening at ' + addr.port);
 	});
-//});
+});
