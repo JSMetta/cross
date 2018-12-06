9bce6038f966c0f9b58172842590398370c348be
clx1608@qq.com <hbdennischen@outlook.com>
2018-12-06 16:50:42 +0800

    init

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-require('dotenv').config();
+require('dotenv').config(),
+	redis = require('redis');
 
 const path = require('path'),
 	connectDb = require('@finelets/hyper-rest/db/mongoDb/ConnectMongoDb'),
 	appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
 	logger = require('@finelets/hyper-rest/app/Logger');
+
+var app = appBuilder.getApp();
+var client = redis.createClient(
+	process.env.REDIS_PORT_6379_TCP_PORT,
+	process.env.REDIS_PORT_6379_TCP_ADDR
+);
+//var client = redis.createClient('6379', 'redis');
+app.get('/', function (req, res, next) {
+	client.incr('counter', function (err, counter) {
+		if (err) return next(err);
+		res.send('This page has been viewed ' + counter + ' times!');
+	});
+});
 
 appBuilder
 	.setWebRoot('/root', './client')
