97a3cbd345d78131c35973f5380368cd7d8993c8
clx1608@qq.com <hbdennischen@outlook.com>
2018-12-06 18:17:36 +0800

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-require('dotenv').config(),
+var express = require('express'),
+	http = require('http'),
 	redis = require('redis');
 
-const path = require('path'),
-	connectDb = require('@finelets/hyper-rest/db/mongoDb/ConnectMongoDb'),
-	appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
-	logger = require('@finelets/hyper-rest/app/Logger');
+var app = express();
 
-var app = appBuilder.getApp();
-var client = redis.createClient(
-	process.env.REDIS_PORT_6379_TCP_PORT,
-	process.env.REDIS_PORT_6379_TCP_ADDR
-);
-//var client = redis.createClient('6379', 'redis');
+console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
+
+// APPROACH 1: Using environment variables created by Docker
+// var client = redis.createClient(
+//      process.env.REDIS_PORT_6379_TCP_PORT,
+//      process.env.REDIS_PORT_6379_TCP_ADDR
+// );
+
+// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
+var client = redis.createClient('6379', 'redis');
+
+
 app.get('/', function (req, res, next) {
 	client.incr('counter', function (err, counter) {
 		if (err) return next(err);
@@ -19,15 +23,6 @@ app.get('/', function (req, res, next) {
 	});
 });
 
-appBuilder
-	.setWebRoot('/root', './client')
-	.setFavicon('client/imgs/favicon.jpg')
-	.end();
-
-//connectDb(function () {
-	logger.info('connect mongodb success .......');
-	var server = appBuilder.run(function () {
-		const addr = server.address();
-		logger.info('the server is running and listening at ' + addr.port);
-	});
-//});
+http.createServer(app).listen(process.env.PORT || 8080, function () {
+	console.log('Listening on port ' + (process.env.PORT || 8080));
+});
