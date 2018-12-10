5c0df11c68bd2175bb6801eb4a4a27c0fd55f958
clx1608@qq.com <hbdennischen@outlook.com>
2018-12-10 17:24:28 +0800

    ic

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ var client = redis.createClient('6379', 'redis')
 app.get('/redis', function (req, res, next) {
 	client.incr('counter', function (err, counter) {
 		if (err) return next(err);
-		var pjson = require('./package.json');
-		res.send('Cross version ' + pjson + ': This page has been viewed ' + counter + ' times!')
+		// var pjson = require('./package.json');
+		res.send('Cross version ' + 'pjson' + ': This page has been viewed ' + counter + ' times!')
 	})
 })
 
