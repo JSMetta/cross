d8eaebc94236c2b4b2dbd7db8da2289fecfa09f9
clx1608@qq.com <hbdennischen@outlook.com>
2018-12-21 23:52:41 +0800

    upload csv

--- server.js
+++ server.js
@@ -2,35 +2,16 @@ require('dotenv').config();
 const connectDb = require('@finelets/hyper-rest/db/mongoDb/ConnectMongoDb'),
 	appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
 	logger = require('@finelets/hyper-rest/app/Logger'),
-	cors = require('cors'),
-	upload = require('multer')({
-		dest: 'uploads/'
-	});
-
-const path = require('path'),
+	path = require('path'),
 	restDir = path.join(__dirname, './server/rests'),
-	resourceDescriptors = require('@finelets/hyper-rest/rests/DirectoryResourceDescriptorsLoader')(restDir),
-	resourceRegistry = require('@finelets/hyper-rest/rests/ResourceRegistry'),
 	graph = require('./server/StateGraph'),
-	transitionsGraph = require('@finelets/hyper-rest/rests/BaseTransitionGraph')(graph, resourceRegistry);
-
-resourceRegistry.setTransitionGraph(transitionsGraph);
-
-/* var app = appBuilder.getApp();
-app.use(cors()) */
-
+	rests = require('@finelets/hyper-rest/rests')(restDir, graph);
+	
 appBuilder
 	.setWebRoot('/cross/root', './client')
 	.setFavicon('client/imgs/favicon.jpg')
-	.setResources(resourceRegistry, resourceDescriptors.loadAll())
+	.setResources(...rests)
 	.end();
-
-/* app.post("/cross/purchases/csv", upload.single('purchases.csv'), function(req, res) {
-	logger.debug('begin to upload')
-	var data = req.file
-	logger.info(JSON.stringify(data))
-	res.send(data)
-}) */
 
 connectDb(function () {
 	logger.info('connect mongodb success .......');
--- server/rests/UploadPurchases.js
+++ server/rests/UploadPurchases.js
@@ -1,8 +1,23 @@
+const Promise = require('bluebird'),
+    logger = require('@finelets/hyper-rest/app/Logger'),
+    CSVStream = require('../../finelets/streams/CSVStream');
+
+const handler = () => {
+    return CSVStream((obj) => {
+        logger.debug('save: ' + JSON.stringify(obj))
+        return Promise.resolve()
+    }, (text) => {
+        logger.debug('parse: ' + text)
+        return {
+            foo: 'foo'
+        }
+    })
+}
+
 module.exports = {
     url: '/cross/purchases/csv',
-    rests: [
-        {
-            type: 'entry'
-        }
-    ]
+    rests: [{
+        type: 'upload',
+        handler: handler
+    }]
 }
