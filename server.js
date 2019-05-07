2e56578713f498f97ac3e8b3e0d790642c9ffb25
clx1608@qq.com <hbdennischen@outlook.com>
2019-05-07 11:07:15 +0800

    remove .env

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const connectDb = require('@finelets/hyper-rest/db/mongoDb/ConnectMongoDb'),
 	appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
 	logger = require('@finelets/hyper-rest/app/Logger'),
