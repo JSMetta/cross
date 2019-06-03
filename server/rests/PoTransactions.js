b754abbabdfb649734f74556097123bc4369c8e3
clx1608@qq.com <hbdennischen@outlook.com>
2019-06-03 18:12:14 +0800

    query purchase transactions

--- server/rests/PoTransactions.js
+++ server/rests/PoTransactions.js
@@ -1,5 +1,15 @@
 const entity = require('../biz/pur/Purchases') 
 
+const list = function (query) {
+    const {id} = query
+    return entity.listSubs(id, 'transactions')
+        .then(function (list) {
+            return {
+                items: list
+            }
+        })
+}
+
 module.exports = {
     url: '/cross/api/pur/purchases/:id/transactions',
     rests: [{
@@ -10,6 +20,11 @@ module.exports = {
                 const type = req.query['type']
                 return entity.doTransaction(id, type, req.body)
             }
+        },
+        {
+            type: 'query',
+            element: 'PoTransaction',
+            handler: list
         }
     ]
 }
